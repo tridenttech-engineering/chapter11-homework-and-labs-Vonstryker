561bbe582b395ee1f5142f410fd56bf9d5b10f0a
Jose Marti <josemarti@my.tridenttech.edu>
2024-08-04 20:18:04 -0400

    Update hw11-21.cpp

--- hw11-21.cpp
+++ hw11-21.cpp
@@ -1,19 +1,22 @@
-//Introductory21.cpp - Displays the average stock price
-//Created/revised by <your name> on <current date>
+// Introductory21.cpp - Displays the average stock price
+// Created/revised by <your name> on <current date>
 
-#include <iostream>
 #include <iomanip>
+#include <iostream>
 using namespace std;
 
-int main()
-{
-	double prices[10] = {96.5, 100.5, 100.5, 100.5, 99, 99, 99, 100, 98.5, 98.9};
-	double total = 0.0;
-	double average = 0.0;
+int main() {
+  double prices[10] = {96.5, 100.5, 100.5, 100.5, 99, 99, 99, 100, 98.5, 98.9};
+  double total = 0.0;
+  double average = 0.0;
 
+  for (int x = 0; x < 10; x += 1) {
+    total += prices[x];
+  }
 
-	cout << fixed << setprecision(2);
-	cout << "Average stock price: $" << average << endl;
-	return 0;
-}	//end of main function
+  average = total / 10;
 
+  cout << fixed << setprecision(2);
+  cout << "Average stock price: $" << average << endl;
+  return 0;
+} // end of main function
